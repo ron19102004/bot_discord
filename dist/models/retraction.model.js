274c09dabba325d27df322ac5aca2c224f6a8f55
Ron <118585933+ron19102004@users.noreply.github.com>
2023-02-20 22:48:11 +0700

    Delete dist directory

--- dist/models/retraction.model.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RetractionModel = void 0;
-class RetractionModel {
-}
-exports.RetractionModel = RetractionModel;
--- dist/routes/index.route.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SheetDatabaseRoute = void 0;
-const sheet_db_route_1 = require("./sheet_db.route");
-Object.defineProperty(exports, "SheetDatabaseRoute", { enumerable: true, get: function () { return sheet_db_route_1.SheetDatabaseRoute; } });
